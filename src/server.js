d43a582832eaa6ce6a2d33e929f5389c82dcc3d8
majedurwd <majedur.cse@gmail.com>
2023-03-02 01:10:02 +0600

    add more

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const shortid = require("shortid")
+const path = require("path")
+const fs = require("fs/promises")
 
 const app = express();
 
@@ -8,6 +11,25 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const dbLocation = path.resolve("src", "data.json")
+
+app.post("/create", async (req, res) => {
+	const player = {
+		...req.body,
+		userId: shortid.generate()
+	}
+	const data = await fs.readFile(dbLocation)
+	const players = JSON.parse(data)
+	players.push(player)
+	
+	await fs.writeFile(dbLocation, JSON.stringify(players))
+
+	res.status(200).json({player})
+})
+
+app.get("/players", (req, res) => {
+	// Start
+})
 
 // API Health Check
 app.get("/health", (_req, res) => {
